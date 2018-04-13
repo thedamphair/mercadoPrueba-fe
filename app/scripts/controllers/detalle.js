9f6b6097708777079a6b326779500a46c2e29780
Azael Job Contreras <azaeljobcontreras@Azaels-MacBook-Pro.local>
2018-04-12 21:37:10 -0500

    Agrega paginación y slide de imagenes

--- app/scripts/controllers/detalle.js
+++ app/scripts/controllers/detalle.js
@@ -13,9 +13,10 @@ angular.module('mercadoApp')
     function init() {
       mercadoservice.getOne(params.id)
       .then(function(succ) {
-        $scope.producto = succ.data
-        $scope.fotos = []
-        for (var i = 0; i < 3; i++) {
+        $scope.producto = angular.copy(succ.data)
+        $scope.imagenActual = 0
+        $scope.fotos = []      
+        for (var i = 0; i < $scope.producto.pictures.length; i++) {
           $scope.fotos.push($scope.producto.pictures[i])
         }
       },function (err) {
@@ -23,4 +24,30 @@ angular.module('mercadoApp')
       })
     }
     init()
+
+    $scope.anterior = function () {
+      if ($scope.imagenActual === 0) {
+        $scope.imagenActual = $scope.fotos.length - 1
+      }
+      else {
+        $scope.imagenActual--
+      }
+    }
+
+    $scope.siguiente = function () {
+      if ($scope.imagenActual === ($scope.fotos.length - 1)) {
+        $scope.imagenActual = 0
+      }
+      else {
+        $scope.imagenActual++
+      }
+    }
+
+    $scope.indexImagenActual = function (index) {
+      return $scope.imagenActual === index
+    }
+
+    $scope.goTo = function (pemalink) {         
+      window.open(pemalink)
+    }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,19 +9,34 @@
  */
 angular.module('mercadoApp')
   .controller('MainCtrl', function ($scope, mercadoservice, $location) {
-    $scope.limit = 50;
-    $scope.offset=0;
+    $scope.limit = 50
+    $scope.offset = 0
+    var aux, resultados, totalPaginas, paginaActiva
 
-    function init() {
+    function calculatePages(total) {      
+      $scope.paginas = []
+      for (var i = 1; i <= 4; i++) {
+        $scope.paginas.push({id:i})
+      }
+    }
+
+    function findResults() {
       mercadoservice.getProducts($scope.limit, $scope.offset)
       .then(function (succ) {
-        $scope.results = succ.data.results;
+        $scope.results = succ.data.results
+        aux = succ.data.results
+        calculatePages(succ.data.paging.total)
       },function () {})
+    }
+
+    function init() {
+      paginaActiva = 1
+      findResults()
     }
     init()
 
     $scope.guardar = function (resultado)  {
-      let params = {
+      var params = {
         id: resultado.id
       }
       mercadoservice.saveProduct(params)
@@ -32,7 +47,37 @@ angular.module('mercadoApp')
       })
     }
 
+    $scope.buscar = function (cadena) {
+      if (cadena === '' || !cadena) {
+        $scope.results = angular.copy(aux)
+        return
+      }
+      var re = new RegExp(''+cadena+'*')
+      var matches = []
+      for (var i = 0; i < $scope.results.length; i++) {
+        if (re.test($scope.results[i].title)) {          
+          matches.push($scope.results[i])
+        }
+      }
+      $scope.results = angular.copy(matches)
+    }
+
     $scope.irGuardados = function () {
       $location.path('/guardados')
     }
+
+    $scope.actual = function(id) {
+      if (id === paginaActiva) {
+        return true
+      }
+      return false
+    }
+
+    $scope.irPagina = function (pagina) {
+      paginaActiva = pagina
+      var auxPag = pagina - 1
+      $scope.offset = auxPag * $scope.limit
+      findResults()
+    }
+
   });
